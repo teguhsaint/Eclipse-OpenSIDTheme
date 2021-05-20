0f1aced26389c3b0f8caf30c10b05dafd31cd5de
Teguh Saint <barayacoffeejombang@gmail.com>
2021-05-20 12:13:45 +0700

    apbdes

--- partials/apbdesa.php
+++ partials/apbdesa.php
@@ -1,11 +1,10 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<section class="container-fluid">
-	<div class="row">
+<section class="container --flex --justify-between --mt-4 --flex-lg-only">
 	<?php foreach($data_widget as $subdata_name => $subdatas) : ?>
-		<div class="col col-sm-6 mb-3">
-			<div class="">
-				<div class="">
+		<div class="column --mx-2">
+			<div class="panel">
+				<div class="panel__header">
 					<h3 class="panel__heading"><?= ($subdatas['laporan'])?></h3>
 				</div>
 				<div class="panel__body">
@@ -13,11 +12,11 @@
 						<?php if($subdata['judul'] != NULL and $key != 'laporan' and $subdata['realisasi'] != 0 or $subdata['anggaran'] != 0): ?>
 							<div class="progress">
 								<label class="progress__title"><?= ucwords(strtolower($subdata['judul'])) ?></label>
-								<div class="--flex --justify-between" style="width: 50%">
+								<div class="--flex --justify-between">
 									<span class="progress__subtitle">Rp<?= number_format($subdata['realisasi']) ?></span>
 									<span class="progress__subtitle">Rp<?= number_format($subdata['anggaran']) ?></span>
 								</div>
-								<div class="progress-bar" style="margin-left: 11%">
+								<div class="progress-bar">
 									<div class="progress-bar__item" style="width:<?= $subdata['persen'] ?>%">
 										<span class="progress-bar__caption"><?= $subdata['persen'] ?>%</span>
 									</div>
@@ -29,5 +28,4 @@
 			</div>
 		</div>
 	<?php endforeach ?>
-	</div>
 </section>
